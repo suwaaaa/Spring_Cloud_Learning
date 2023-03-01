456114e7f84f5eed068bbb727f0562e36f2fbcef
suwaaaa <1264584869@qq.com>
2023-03-01 23:09:46 +0800

    Add spring gateway module (simple)

---
+++ Spring_Gateway_Module/src/main/java/nacos/major/GatewayApplication.java
@@ -0,0 +1,19 @@
+package nacos.major;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Java_IBM_Learning nacos.major
+ *
+ * @author 12645
+ * @createTime 2023/3/1 22:08
+ * @description
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( GatewayApplication.class, args);
+    }
+}
